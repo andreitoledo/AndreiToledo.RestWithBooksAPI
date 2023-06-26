8af7bb433abc14e59b72e2ac1650bc424b2788d9
andreitoledo <andreiltoledo@hotmail.com>
2023-06-26 17:55:56 -0300

    Definindo a Interface IPersonService

---
+++ Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using AndreiToledo.RestWithBooksAPI.Model;
+using System.Collections.Generic;
+
+namespace AndreiToledo.RestWithBooksAPI.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
