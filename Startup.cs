df83d26eaee695e927ec165f964534a0b4352106
andreitoledo <andreiltoledo@hotmail.com>
2023-06-27 16:51:49 -0300

    Adicionando Suporte à Migrations

--- Startup.cs
+++ Startup.cs
@@ -9,27 +9,43 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AndreiToledo.RestWithBooksAPI.Repository;
 using AndreiToledo.RestWithBooksAPI.Repository.Implementations;
+using Serilog;
+using System.Collections.Generic;
+using System;
 
 namespace AndreiToledo.RestWithBooksAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
 
         public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
 
             // Configuração com o Banco de Dados
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
 
             // Versionamento API
             services.AddApiVersioning();
@@ -38,6 +54,8 @@ namespace AndreiToledo.RestWithBooksAPI
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -57,6 +75,25 @@ namespace AndreiToledo.RestWithBooksAPI
             {
                 endpoints.MapControllers();
             });
+        }       
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }           
         }
     }
 }
