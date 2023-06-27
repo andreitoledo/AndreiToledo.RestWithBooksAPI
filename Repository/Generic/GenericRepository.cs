d7c94b6683759b06bb133605e2efde9fbbc8f7e4
andreitoledo <andreiltoledo@hotmail.com>
2023-06-27 20:31:53 -0300

    Implementando a Estrutura Base do Repositório Genérico

---
+++ Repository/Generic/GenericRepository.cs
@@ -0,0 +1,34 @@
+﻿using AndreiToledo.RestWithBooksAPI.Model.Base;
+using System;
+using System.Collections.Generic;
+
+namespace AndreiToledo.RestWithBooksAPI.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindByID(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/Generic/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using AndreiToledo.RestWithBooksAPI.Model;
+using AndreiToledo.RestWithBooksAPI.Model.Base;
+using System.Collections.Generic;
+
+namespace AndreiToledo.RestWithBooksAPI.Repository
+
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T FindByID(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long id);
+    }
+}
