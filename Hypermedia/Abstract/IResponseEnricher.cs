cea022c71596328243e77473c2d342398f77c124
andreitoledo <andreiltoledo@hotmail.com>
2023-06-28 11:12:02 -0300

    Criando a Interface e Constantes para gerar o HATEOAS

---
+++ Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace AndreiToledo.RestWithBooksAPI.Hypermedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutedContext context);
+
+        Task Enrich(ResultExecutedContext context);
+    }
+}
---
+++ Hypermedia/Abstract/ISupportHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AndreiToledo.RestWithBooksAPI.Hypermedia.Abstract
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
