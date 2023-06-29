5b31349d0a339e27e555a3b41b8f195c4c794a4f
andreitoledo <andreiltoledo@hotmail.com>
2023-06-29 20:26:54 -0300

    Implementando o PersonRepository

---
+++ Repository/PersonRepository.cs
@@ -0,0 +1,35 @@
+﻿using AndreiToledo.RestWithBooksAPI.Model;
+using AndreiToledo.RestWithBooksAPI.Model.Context;
+using AndreiToledo.RestWithBooksAPI.Repository.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndreiToledo.RestWithBooksAPI.Repository
+{
+    public class PersonRepository : GenericRepository<Person>, IPersonRepository
+    {
+        public PersonRepository(MySQLContext context) : base (context) { }
+
+        public Person Disable(long id)
+        {
+            if (!_context.Persons.Any(p => p.Id.Equals(id))) return null;
+            var user = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (user != null)
+            {
+                user.Enabled = false;
+                try
+                {
+                    _context.Entry(user).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return user;
+        }
+    }
+}
