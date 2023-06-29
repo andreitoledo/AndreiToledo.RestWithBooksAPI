3de051b1bc9e4d397ed12ffa90754ea51f30d2bf
andreitoledo <andreiltoledo@hotmail.com>
2023-06-29 13:13:48 -0300

    Implementando as Classes de VO e Business

---
+++ Business/ILoginBusiness.cs
@@ -0,0 +1,13 @@
+﻿using AndreiToledo.RestWithBooksAPI.Data.VO;
+
+namespace AndreiToledo.RestWithBooksAPI.Business
+{
+    public interface ILoginBusiness
+    {
+        TokenVO ValidateCredentials(UserVO user);
+
+        //TokenVO ValidateCredentials(TokenVO token);
+
+        //bool RevokeToken(string userName);
+    }
+}
