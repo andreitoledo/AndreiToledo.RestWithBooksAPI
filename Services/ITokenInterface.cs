212cafcd385f21709b11eaba79002ce589dae1b6
andreitoledo <andreiltoledo@hotmail.com>
2023-06-28 22:50:53 -0300

    Implementando o Método Responsável por Atualizar as Informações do Usuário

---
+++ Services/ITokenInterface.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace AndreiToledo.RestWithBooksAPI.Services
+{
+    public interface ITokenInterface
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+
+    }
+}
