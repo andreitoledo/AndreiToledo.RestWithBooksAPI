dd4cdc1df7eee7f6ac33f0df9e7ebf41debb1e09
andreitoledo <andreiltoledo@hotmail.com>
2023-06-29 22:51:49 -0300

    Ajustando o PersonRepository

--- Repository/IPersonRepository.cs
+++ Repository/IPersonRepository.cs
@@ -1,10 +1,12 @@
 ﻿using AndreiToledo.RestWithBooksAPI.Model;
+using System.Collections.Generic;
 
 namespace AndreiToledo.RestWithBooksAPI.Repository
 {
     public interface IPersonRepository : IRepository<Person>
     {
         Person Disable(long id);
-        
+        List<Person> FindByName(string firstName, string secondName);
+
     }
 }
